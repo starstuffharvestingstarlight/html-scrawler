3a9c75bc47711fe67773fe903b4de2d4e584ddac
starstuffharvestingstarlight <joao@hipercubo.net>
2014-06-18 22:12:12 +1000

    Angularised the file input (what a mess)

--- app/js/app.js
+++ app/js/app.js
@@ -1,37 +1,6 @@
 'use strict';
 
-// TODO: clean up and use data binding properly
-var imageLoader = {
-	init: function(fileInputId, canvasId) {
-		if (!this.ctx) {
-			document.
-				getElementById(fileInputId).
-				addEventListener('change', this.handler, false);
-			this.canvas = document.
-				getElementById(canvasId);
-			this.ctx = this.canvas.getContext('2d');
-		}
-	},
-	handler: function(e) {
-		/**
-		@see https://stackoverflow.com/questions/10906734/how-to-upload-image-into-html5-canvas
-		@see http://jsfiddle.net/influenztial/qy7h5/
-		*/
-		try {
-			var reader = new FileReader();
-			reader.onload = function(event){
-				var img = new Image();
-				img.onload = function(){
-					imageLoader.ctx.drawImage(img, 0, 0, imageLoader.canvas.width, imageLoader.canvas.height);
-				}
-				img.src = event.target.result;
-			}
-			reader.readAsDataURL(e.target.files[0]);
-		} catch (ex) { }
-	}
-};
-
-angular.
+var app = angular.
 	module('scrawler', ['ngRoute']).
 	config(['$routeProvider', function($routeProvider) {
 		$routeProvider.when('/load', {
@@ -45,12 +14,62 @@ angular.
 		$routeProvider.otherwise({
 			redirectTo: '/load'
 		});
-	}]).
+	}])
+;
+
+app.
 	controller('imageLoaderController', ['$scope', function($scope) {
-		imageLoader.init('imageLoader', 'imageCanvas');
-		console.log('loader');
-	}]).
+		$scope.preview = {
+			file: null,
+			width: 100,
+			height: 100,
+			update: function() {
+				/**
+				@see https://stackoverflow.com/questions/10906734/how-to-upload-image-into-html5-canvas
+				@see http://jsfiddle.net/influenztial/qy7h5/
+				*/
+				try {
+					var img = new Image();
+					img.onload = function() {
+						document.
+						getElementById('imageCanvas').
+						getContext('2d').
+						drawImage(img, 0, 0, $scope.preview.width, $scope.preview.height);
+					}
+					img.src = this.file;
+				} catch (ex) {
+					console.log(ex);
+				}
+			}
+		}
+	}])
+;
+
+app.
 	controller('textRendererController', ['$scope', function($scope) {
 		console.log('FIXME');
+	}])
+;
+
+// @see https://stackoverflow.com/questions/17063000/ng-model-for-input-type-file
+app.
+	directive('fileread', [function() {
+		return {
+			scope: {
+				fileread: '='
+			},
+			link: function (scope, element, attributes) {
+				element.bind('change', function (changeEvent) {
+					var reader = new FileReader();
+					reader.onload = function (loadEvent) {
+						scope.$apply(function () {
+							scope.$parent.preview.file = loadEvent.target.result;
+						});
+						scope.$parent.preview.update();
+					}
+					reader.readAsDataURL(changeEvent.target.files[0]);
+				});
+			}
+		}
 	}])
 ;
