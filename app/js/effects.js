c26e7b05eafcd7451fd803bcdb6c9f12614593aa
starstuffharvestingstarlight <joao@hipercubo.net>
2014-06-24 00:11:04 +1000

    first working scrawl: utf chars

--- app/js/effects.js
+++ app/js/effects.js
@@ -3,7 +3,34 @@ app.
 		return [
 			{
 				process: function (ctx) {
-					this.result = $sce.trustAsHtml('<em>Placeholder for effect 1</em>');
+					w = ctx.canvas.width
+					h = ctx.canvas.height
+					inData = ctx.getImageData(0,0,w,h)
+					outctx = document.getElementById('debugCanvas').getContext('2d')
+					outctx.putImageData(inData, 0, 0)
+					outData = outctx.getImageData(0,0,w,h)
+					/** async
+					P = new Pixastic(outctx, 'lib/vendor/pixastic/')
+					P.posterize({levels: 4}).done(function() {})
+					*/
+					Pixastic.Effects.posterize(inData.data, outData.data, w, h, {levels: 2})
+					Pixastic.Effects.desaturate(outData.data, outData.data, w, h)
+					outctx.putImageData(outData, 0, 0)
+					console.log(outData.data)
+
+					// TODO: replace with distinct b/w values
+					blocks = {
+						255: '█',
+						227: '▓',
+						76:  '▒',
+						0:   '░',
+					}
+					str = ''
+					for (var i = 0, n = w * h * 4, c = 0; i < n; i+=4, c++) {
+						str += blocks[outData.data[i]] || ' '
+						if (c % w == w-1) str += '\n'
+					}
+					this.result = $sce.trustAsHtml('<pre style="font-size:.5em">' + str + '</pre>');
 				}
 			},
 			{
